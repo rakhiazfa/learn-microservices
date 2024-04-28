1854806f726028dc576af327e814848bbbc2f15d
rakhi <rakhiazfa0421@gmail.com>
2024-04-28 10:51:12 +0700

    Remove /api prefix.

--- identity-service/bootstrap/app.php
+++ identity-service/bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__ . '/../routes/api.php',
+        apiPrefix: '/',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
