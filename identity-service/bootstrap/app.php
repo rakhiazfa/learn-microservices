f4f3d890f270b22e1512b2a68572c0faceedd41b
rakhi <rakhiazfa0421@gmail.com>
2024-04-30 13:18:31 +0700

    trust proxies

--- identity-service/bootstrap/app.php
+++ identity-service/bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\Authentication;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'authentication' => Authentication::class,
         ]);
+        $middleware->trustProxies(
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
