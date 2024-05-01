95e6631293851247c79d23a2f2ff6582f1e431aa
rakhi <rakhiazfa0421@gmail.com>
2024-05-01 13:10:48 +0700

    fix: menu request schema.

--- master-data-service/src/schemas/create-menu.schema.ts
+++ master-data-service/src/schemas/create-menu.schema.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, ValidateIf } from "class-validator";
+import { IsNotEmpty, IsNumber, IsOptional, ValidateIf } from "class-validator";
 
 class CreateMenuSchema {
     @IsNotEmpty()
@@ -9,11 +9,14 @@ class CreateMenuSchema {
 
     @ValidateIf((schema) => schema.order !== null || schema.order !== undefined)
     @IsNumber()
+    @IsOptional()
     order?: number;
+
     @ValidateIf(
         (schema) => schema.parent_id !== null || schema.parent_id !== undefined
     )
     @IsNumber()
+    @IsOptional()
     parent_id?: number;
 }
 
