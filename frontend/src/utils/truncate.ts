e071e2bcaa594ea73ddaa40cc443b7d31677afe7
rakhi <rakhiazfa0421@gmail.com>
2024-05-24 15:40:43 +0700

    Create truncate string helper function.

---
+++ frontend/src/utils/truncate.ts
@@ -0,0 +1,9 @@
+const truncate = (str: string, num: number): string => {
+    if (str.length > num) {
+        return str.slice(0, num) + " ...";
+    } else {
+        return str;
+    }
+};
+
+export default truncate;
